96ea879fbc5766b556e516f095b3cfaa5ca987a7
Rahim Alizada <rahim.alizada@gmail.com>
2020-04-16 11:45:05 +0400

    Remove auto generated component and service

--- projects/rahimalizada/ngx-external-script-loader/src/lib/ngx-external-script-loader.module.ts
+++ projects/rahimalizada/ngx-external-script-loader/src/lib/ngx-external-script-loader.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
-import { NgxExternalScriptLoaderComponent } from './ngx-external-script-loader.component';
-
-
 
 @NgModule({
-  declarations: [NgxExternalScriptLoaderComponent],
-  imports: [
-  ],
-  exports: [NgxExternalScriptLoaderComponent]
+  declarations: [],
+  imports: [],
+  exports: [],
 })
-export class NgxExternalScriptLoaderModule { }
+export class NgxExternalScriptLoaderModule {}
--- projects/rahimalizada/ngx-external-script-loader/src/public-api.ts
+++ projects/rahimalizada/ngx-external-script-loader/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-external-script-loader
  */
 
-export * from './lib/ngx-external-script-loader.service';
-export * from './lib/ngx-external-script-loader.component';
 export * from './lib/ngx-external-script-loader.module';
